33e5895e3580deb570f0868066fb8b2cb63e34a8
MonG_Bana <jjasu0826@gmail.com>
2019-11-12 17:58:36 +0900

    #3.1 All you need to know about State

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
 
-function Food({name}) {
-  console.log(name);
-return <h1>this food is {name}</h1>
-}
+class App extends Component {
+  state = {
+    count: 0
+  };
 
-const MyFood = [
-   {id:0, name:"chicken", rating:5}
-  ,{id:1, name:"pizza", rating:4.7}
-  ,{id:2, name:"ramen", rating:3.9}
-  ,{id:3, name:"chicken soup", rating:2.8}
-  ,{id:4, name:"potato", rating:3}
-];
+  add = () => {
+    console.log("push add button");
+    this.setState(current=>({
+      count: current.count + 1
+    }));
+  }
 
-function renderFood(dish) {
-  console.log(dish);
-  return <Food key={dish.id} name={dish.name}/>
-}
+  minus = () => {
+    console.log("push minus button");  
+    this.setState(current=>({
+      count: current.count - 1 < 0
+        ? 0
+        : current.count - 1
+    }));
+  }
 
-function App() {
-  return (
+  render(){
+    return (
     <div>
-      {MyFood.map(renderFood)}
+      <h1>Number : {this.state.count}</h1>
+      <button onClick={this.add}> + </button>
+      <button onClick={this.minus}> - </button>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
