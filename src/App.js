1ba110c5084f0fb7492f12a57f77accaa85bf509
MonG_Bana <jjasu0826@gmail.com>
2019-11-13 18:00:04 +0900

    #4.4 Styles Timelapse

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React, {Component} from 'react';
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 class App extends Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() =>{
+    const {data: {data:{movies} } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading:false});
   };
 
-  add = () => {
-    console.log("push add button");
-    this.setState(current=>({
-      count: current.count + 1
-    }));
-  }
-
-  minus = () => {
-    console.log("push minus button");  
-    this.setState(current=>({
-      count: current.count - 1 < 0
-        ? 0
-        : current.count - 1
-    }));
+  componentDidMount(){
+    this.getMovies();
   }
 
   render(){
+    const {isLoading, movies} = this.state;
     return (
-    <div>
-      <h1>Number : {this.state.count}</h1>
-      <button onClick={this.add}> + </button>
-      <button onClick={this.minus}> - </button>
-    </div>
+    <section className="container">
+      {isLoading 
+        ? ( <div className="loader">
+              <span className="loader_text">Loading...</span>
+            </div> 
+          )
+        : (
+          <div className="movies">
+            {movies.map(movie=>(
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+    </section>
     );
   }
 }
